730220dc5450039ab443586b53903e1a5851b994
Ceril Heyrosa <cerilheyrosa@gmail.com>
2024-09-01 18:25:38 +0800

    Intial Commit: Queue

---
+++ Queue/queue.h
@@ -0,0 +1,14 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+typedef struct node{
+    char data;
+    struct node* next;
+}List;
+
+typedef struct{
+    List *front;
+    List *rear;
+}Queue;
+
+void init(Queue)
