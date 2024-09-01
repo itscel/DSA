cfbf56f2cac8e43b962b94a24fde19a361f5daee
Ceril Heyrosa <cerilheyrosa@gmail.com>
2024-09-01 15:30:42 +0800

    Recent Commit

---
+++ cursorbase.h
@@ -0,0 +1,24 @@
+#ifndef CURSORBASE_H
+#define CURSORBASE_H
+#include <stdio.h>
+#define MAX 10
+
+typedef struct{
+    char data;
+    int next;
+}Node;
+
+typedef struct{
+    Node elem[MAX];
+    int avail;
+}CBase;
+
+void init(CBase *CB){
+    int i;
+
+
+    for(i=MAX-1;i>=0;i--){
+        CB->elem[i].next=i-1;
+    }
+    CB->avail=9;
+}
